6a57042a03150c7df145db4a65e3ac43dd1fa097
Bianca Danforth <biancadanforth@gmail.com>
2017-04-22 15:38:13 -0700

    Update value of each tile with random char from each set
    Each random char is from a random die, picking dies without replacement.

--- src/scripts/components/Board.jsx
+++ src/scripts/components/Board.jsx
@@ -2,6 +2,23 @@ import React  from 'react';
 
 import Square from './Square.jsx';
 
+const configurations = ['aaafrs', 'aaeeee', 'aafirs', 'adennn', 'aeeeem', 'aeegmu', 'aegmnn', 'afirsy', 'bjkqxz', 'ccenst', 'ceiilt', 'ceilpt', 'ceipst', 'ddhnot', 'dhhlor', 'dhlnor', 'dhlnor', 'eiiitt', 'emottt', 'ensssu', 'fiprsy', 'gorrvw', 'iprrry', 'nootuw', 'ooottu'];
+
+let newConfigurations = configurations.slice();
+let charArray = [];
+for (let i = 0; i < configurations.length; i++) {
+	let configIndex = Math.floor(Math.random()*newConfigurations.length);
+	let configString = newConfigurations[configIndex];
+	// remove result from array so it doesn't recurr
+	newConfigurations.splice(configIndex, 1);
+	let configCharIndex = Math.floor(Math.random()*configString.length);
+	let configChar = configString.charAt(configCharIndex).toUpperCase();
+	if (configChar === 'Q') {
+		configChar = 'Qu';
+	}
+	charArray.push(configChar);
+}
+
 class Board extends React.Component {
 
 	constructor() {
@@ -48,48 +65,48 @@ class Board extends React.Component {
 		}
 	}
 
-  renderSquare(id) {
+  renderSquare(id, char) {
   	let newSelected = this.state.selected.slice();
-  	return <Square value={id} lastClicked={(id) => this.handleLastClicked(id)} isSelected={newSelected[id]}/>;
+  	return <Square id={id} value={char} lastClicked={(id) => this.handleLastClicked(id)} isSelected={newSelected[id]}/>;
   }
   
   render() {
     return(
       <div>
       <div className="row">
-      	{this.renderSquare(0)}
-      	{this.renderSquare(1)}
-      	{this.renderSquare(2)}
-      	{this.renderSquare(3)}
-      	{this.renderSquare(4)}
+      	{this.renderSquare(0, charArray[0])}
+      	{this.renderSquare(1, charArray[1])}
+      	{this.renderSquare(2, charArray[2])}
+      	{this.renderSquare(3, charArray[3])}
+      	{this.renderSquare(4, charArray[4])}
       </div>
       <div className="row">
-      	{this.renderSquare(5)}
-      	{this.renderSquare(6)}
-      	{this.renderSquare(7)}
-      	{this.renderSquare(8)}
-      	{this.renderSquare(9)}
+      	{this.renderSquare(5, charArray[5])}
+      	{this.renderSquare(6, charArray[6])}
+      	{this.renderSquare(7, charArray[7])}
+      	{this.renderSquare(8, charArray[8])}
+      	{this.renderSquare(9, charArray[9])}
       </div>
       <div className="row">
-      	{this.renderSquare(10)}
-      	{this.renderSquare(11)}
-      	{this.renderSquare(12)}
-      	{this.renderSquare(13)}
-      	{this.renderSquare(14)}
+      	{this.renderSquare(10, charArray[10])}
+      	{this.renderSquare(11, charArray[11])}
+      	{this.renderSquare(12, charArray[12])}
+      	{this.renderSquare(13, charArray[13])}
+      	{this.renderSquare(14, charArray[14])}
       </div>
       <div className="row">
-      	{this.renderSquare(15)}
-      	{this.renderSquare(16)}
-      	{this.renderSquare(17)}
-      	{this.renderSquare(18)}
-      	{this.renderSquare(19)}
+      	{this.renderSquare(15, charArray[15])}
+      	{this.renderSquare(16, charArray[16])}
+      	{this.renderSquare(17, charArray[17])}
+      	{this.renderSquare(18, charArray[18])}
+      	{this.renderSquare(19, charArray[19])}
       </div>
       <div className="row">
-      	{this.renderSquare(20)}
-      	{this.renderSquare(21)}
-      	{this.renderSquare(22)}
-      	{this.renderSquare(23)}
-      	{this.renderSquare(24)}
+      	{this.renderSquare(20, charArray[20])}
+      	{this.renderSquare(21, charArray[21])}
+      	{this.renderSquare(22, charArray[22])}
+      	{this.renderSquare(23, charArray[23])}
+      	{this.renderSquare(24, charArray[24])}
       </div>
       </div>
     )
