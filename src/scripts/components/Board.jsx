88a73e1472a55766ac9f57c62d651a0123317919
Bianca Danforth <biancadanforth@gmail.com>
2017-04-22 14:47:33 -0700

    Implement adjacent squares criterion
     After the first selected square, the user can only select squares that are adjacent horizontally, vertically, or diagonally to the last selected square

--- src/scripts/components/Board.jsx
+++ src/scripts/components/Board.jsx
@@ -8,11 +8,49 @@ class Board extends React.Component {
 		super();
 		this.state = {
 			lastClicked: null,
+			selected: Array.apply(null, Array(25)).map(Boolean.prototype.valueOf, false),
+			history: []
 		}
 	}
 
-  renderSquare(i) {
-  	return <Square value={i} lastClicked={(value) => this.setState({lastClicked: value})} lastClickedId={this.state.lastClicked} />;
+	handleLastClicked(id) {
+		// can't assign directly into elements of an object in setState, i.e. this.setState({selected[id]: true}) doesn't work.
+		let newSelected = this.state.selected.slice();
+		const prevId = this.state.lastClicked;
+		if (prevId) {
+			const adjTileIds = [prevId, prevId-6, prevId-5, prevId-4, prevId-1, prevId+1, prevId+4, prevId+5, prevId+6];
+			// if the clicked tile is adjacent to the last clicked tile:
+			if (adjTileIds.indexOf(id) !== -1) {
+				// if tile is not selected, select it and update lastClickedId
+				if (!(newSelected[id])) {
+					newSelected[id] = true;
+					this.setState((prevState, props) => {
+						prevState.history.push(id);
+						return {lastClicked: id, selected: newSelected, history: prevState.history};
+					});
+				// if tile is selected AND it was the last clicked tile, deselect it and update lastClicked to have its previous value
+				} else if (id === this.state.lastClicked) {
+					newSelected[id] = false;
+					this.setState((prevState, props) => {
+						// pop the last element off history
+						prevState.history.pop();
+						let lastLastClicked = prevState.history[prevState.history.length -1];
+						return {lastClicked: lastLastClicked , selected: newSelected, history: prevState.history};
+					});
+				}
+			}
+		} else {
+			newSelected[id] = true;
+			this.setState((prevState, props) => {
+				prevState.history.push(id);
+				return {lastClicked: id, selected: newSelected, history: prevState.history};
+			});
+		}
+	}
+
+  renderSquare(id) {
+  	let newSelected = this.state.selected.slice();
+  	return <Square value={id} lastClicked={(id) => this.handleLastClicked(id)} isSelected={newSelected[id]}/>;
   }
   
   render() {
--- src/scripts/components/Square.jsx
+++ src/scripts/components/Square.jsx
@@ -1,28 +1,11 @@
 import React from 'react';
 
 class Square extends React.Component {
-  
-	constructor() {
-		super();
-		this.state = {
-			selected: false,
-		};
-	}
-
-	handleClick() {
-		// if tile is not selected, select it and update lastClickedId
-		if (!(this.state.selected)) {
-			this.setState({selected: true});
-			this.props.lastClicked(this.props.value);
-		}
-		// if tile is selected AND it was the last clicked tile, deselect it
-		(this.state.selected && this.props.value === this.props.lastClickedId) ? this.setState({selected: false}) : null;
-	}
 
   render() {
     return(
-      <div className= {this.state.selected ? "tile tile-selected" : "tile"} onClick={this.handleClick.bind(this)}>
-      <div>{this.state.value}</div>
+      <div className= {this.props.isSelected ? "tile tile-selected" : "tile"} onClick={() => this.props.lastClicked(this.props.value)}>
+      <div>{this.props.value}</div>
       </div>
     )
   }
